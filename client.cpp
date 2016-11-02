e204123cfce9b3beb1cd0af7fc10d1c82090ad9f
TrofimovaEkaterina <onesen@yandex.ru>
2016-11-03 05:54:33 +0700

    FTP (send all)

--- client.cpp
+++ client.cpp
@@ -1,65 +1,183 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+//#include <cstdlib>
 #include <string.h>
+#include <string>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
 #include "opt.h"
 
-#define N 1000
+
+#include <sys/stat.h>
+
+
+int send_to_the_death(int serv_sock, char* buf, int n_to_send);
 
 int main(int argc, char **argv) {
 
-    int sockfd, portno, n;
+    int serv_sock, portno, ret_snd;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    char buffer[256];
+    char* fn_length_buf;
 
-    if (argc < 2) {
-        fprintf(stderr, "usage %s hostname port\n", argv[0]);
-        exit(0);
+    FILE* fptr;
+
+    /***********************************************************/
+    /* Check for correct arguments number                      */
+    /***********************************************************/
+    if (argc < 4) {
+        fprintf(stderr, "usage %s hostname port filename\n", argv[0]);
+        exit(TOO_FEW_ARG);
     }
 
-    portno = DEF_PORT; //atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (sockfd < 0)
-        error("ERROR opening socket", 1);
-
-    /*server = gethostbyname(argv[1]);
+    /***********************************************************/
+    /* Get hostent structure to connect the server             */
+    /***********************************************************/
+    server = gethostbyname(argv[1]);
 
     if (server == NULL) {
-        fprintf(stderr, "ERROR, no such host\n");
-        exit(0);
+        herror("gethostbyname() failed");
+        fprintf(stderr, "Terminating...\n");
+        exit(SERV_ADDR_ERR);
     }
-     */
 
+    portno = atoi(argv[2]);
+
+
+    /***********************************************************/
+    /* Check if file for transfer exsists and access for       */
+    /* reading                                                 */
+    /***********************************************************/
+    if (access(argv[3], R_OK) != 0) {
+        fprintf(stdout, "file with name %s not exsists or there is no permission to read it. Transmission cancelation...\n", argv[3]);
+        exit(ERR_FILE_ACCESS);
+    }
+
+    /***********************************************************/
+    /* Open the file                                           */
+    /***********************************************************/
+    if ((fptr = fopen(argv[3], "rb")) == NULL) {
+        error("fopen() failed");
+    }
+
+    struct stat st;
+    if (stat(argv[3], &st) < 0) {
+        error("stat() failed");
+    }
+
+    long int notWrited = st.st_size;
+    printf("%ld\n", notWrited);
+
+    /*************************************************************/
+    /* Create an AF_INET stream socket to connect to server      */
+    /*************************************************************/
+    serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (serv_sock < 0)
+        error("socket() failed");
+
+    //зануляем структуру адреса
     bzero((char *) &serv_addr, sizeof (serv_addr));
 
+    //вносим адрес хоста
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-    //bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length);
+    bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
 
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof (serv_addr)) < 0)
-        error("ERROR connecting", 2);
+    /*************************************************************/
+    /* Client trys to connect server                             */
+    /*************************************************************/
+    if (connect(serv_sock, (struct sockaddr *) &serv_addr, sizeof (serv_addr)) < 0)
+        error("connect() failed");
 
-    printf("Please enter the message: ");
-    bzero(buffer, 256);
-    fgets(buffer, 255, stdin);
+    /*************************************************************/
+    /* Make msg with file_name_length                            */
+    /*************************************************************/
+    int fn_length = strlen(argv[3]);
+    fn_length_buf = (char*) calloc(5, sizeof (char));
 
-    //for (int i = 0; i < N; i++) {
-    while (true) {
-        n = write(sockfd, buffer, strlen(buffer));
-        if (n < 0)
-            error("ERROR writing to socket", 3);
-        usleep(1000);
+    sprintf(fn_length_buf, "%i\0", fn_length);
+
+    /*************************************************************/
+    /* File name length sending                                  */
+    /*************************************************************/
+    ret_snd = send_to_the_death(serv_sock, fn_length_buf, 4 * sizeof (char));
+    printf("%d\n", ret_snd);
+
+    if (ret_snd == SEND_ERR) {
+        perror("send() failed");
+        close(serv_sock);
+        return errno;
+    }
+
+    /*************************************************************/
+    /* File name sending                                         */
+    /*************************************************************/
+    ret_snd = send_to_the_death(serv_sock, argv[3], fn_length);
+    printf("%s\n", argv[3]);
+
+    if (ret_snd == SEND_ERR) {
+        perror("send() failed");
+        close(serv_sock);
+        return errno;
+    }
+
+    char* buf = (char*) calloc(BUFSIZE + 1, sizeof (char));
+
+    /*************************************************************/
+    /* File content sending                                      */
+    /*************************************************************/
+    while (notWrited > 0) {
+
+        int ret_frd;
+
+        ret_frd = fread(buf, 1, BUFSIZE, fptr);
+        printf("%d\n", ret_frd);
+
+        if (ret_frd < 0) {
+            error("fread() failed");
+        }
+
+        ret_snd = send_to_the_death(serv_sock, buf, ret_frd);
+        printf("%d\n", ret_snd);
+
+        if (ret_snd == SEND_ERR) {
+            perror("send() failed");
+            close(serv_sock);
+            return errno;
+        }
+
+        notWrited -= ret_frd;
 
     }
 
-    close(sockfd);
+    printf("File has been successfuly sended\n");
+
+    close(serv_sock);
     return 0;
+}
+
+int send_to_the_death(int serv_sock, char* buf, int n_to_send) {
+
+    int ret;
+    int offset = 0;
+
+    do {
+
+        ret = send(serv_sock, &buf[offset], n_to_send, 0);
+        printf("%d\n", ret);
+
+        if (ret < 0) {
+            return SEND_ERR;
+        }
+
+        n_to_send -= ret;
+
+    } while (n_to_send > 0);
+
+    return SUCCESS;
 }
--- opt.h
+++ opt.h
@@ -1,14 +1,28 @@
 #ifndef OPT_H
 #define OPT_H
 
-#define DEF_BC_PORT 14204
-#define MSG_SIZE 17          
+#define DEF_PORT 14204
+#define BUFSIZE 256 
+#define MAX_FULL_NAME_LENGTH 200
+#define MAX_CONN_N 10   /*max number of connections*/
+
+#define SEND_ERR -6
+#define ERR_FILE_ACCESS -5
+#define SERV_ADDR_ERR -4
+#define TOO_FEW_ARG -3
+#define FILE_WR_ERR -2
+#define SUCCESS 0
+#define CONN_CLOSED_BY_CLI 1
+#define INVALID_NAME_LENGTH 2
+#define MEM_ALLOC_FAIL 3
+
 #include <unistd.h>
+#include <errno.h>
 #include <stdlib.h>
 
-void error(const char *msg, int code) {
+void error(const char *msg) {
     perror(msg);
-    exit(code);
+    exit(errno);
 }
 
 
